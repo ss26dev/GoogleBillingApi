f21a562e2b9098b62ad227ee200f4537b12a54ec
adriansulumberchean <cvscvscvs>
2021-11-27 15:18:05 +0200

    [refactor] Add documentation to ILocalSkuDetails.getFree24Timestamp and setFreeFor24h

--- GoogleBillingApiLib/src/main/java/com/softstackdev/googlebilling/ILocalSkuDetails.kt
+++ GoogleBillingApiLib/src/main/java/com/softstackdev/googlebilling/ILocalSkuDetails.kt
@@ -2,7 +2,16 @@ package com.softstackdev.googlebilling
 
 interface ILocalSkuDetails {
 
+    /**
+     * @return the timestamp when the sku was procured via Free/24h option
+     */
     fun getFree24Timestamp(skuName: String): Long
+
+    /**
+     * Set skuName as free for 24 hours
+     *
+     * @return the current timestamp at which the sku was set as free
+     */
     fun setFreeFor24h(skuName: String): Long
 
     fun isPurchased(skuName: String): Boolean
